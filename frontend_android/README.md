865ecc4ba4a269fa1002a6fa2c1daefe97de88bb
oliver gitte <oliver.gitte@gmail.com>
2024-03-31 16:06:26 -0400

    frontend

---
+++ frontend_android/README.md
@@ -0,0 +1,3 @@
+# frontend_android
+
+A new Flutter project.
---
+++ frontend_android/android/app/src/main/kotlin/com/example/frontend_android/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.frontend_android
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
