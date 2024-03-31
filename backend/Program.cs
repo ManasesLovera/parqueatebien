d9ab9e29441ff2e9db3bac2208c214cd1a2debaf
oliver gitte <oliver.gitte@gmail.com>
2024-03-31 16:38:43 -0400

    WIP

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.Services.AddCors(options =>
+{
+  options.AddDefaultPolicy(
+      policy =>
+      {
+        policy.SetIsOriginAllowed(origin => new Uri(origin).IsLoopback);
+      });
+});
 
+var app = builder.Build();
+app.UseCors();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
--- frontend_android/lib/main.dart
+++ frontend_android/lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  const app = MainApp();
+  app.pokeUri();
+  runApp(app);
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
+
+  Future<void> pokeUri() async {
+    final httpPackageUrl = Uri.http('localhost:8089', '/');
+    final httpPackageInfo = await http.read(httpPackageUrl);
+    print(httpPackageInfo);
+  }
 
   @override
   Widget build(BuildContext context) {
