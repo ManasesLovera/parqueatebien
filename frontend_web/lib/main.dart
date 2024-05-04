26f7a3d54b94625832e94e38a84f7cb8eea340d3
cesar augusto moreno <161043574+CesarMoreno482@users.noreply.github.com>
2024-05-04 18:30:56 -0400

    ahi ta

--- frontend_web/lib/main.dart
+++ frontend_web/lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void main() async {
+void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatefulWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   _MainAppState createState() => _MainAppState();
@@ -15,7 +15,9 @@ class MainApp extends StatefulWidget {
 
 class _MainAppState extends State<MainApp> {
   final TextEditingController _licensePlateController = TextEditingController();
-  List<Incauto> _incautos = [];
+  List<Citizen> _citizens = [];
+  bool _isLoading = false;
+  String _errorMessage = '';
 
   @override
   void dispose() {
@@ -23,35 +25,50 @@ class _MainAppState extends State<MainApp> {
     super.dispose();
   }
 
-  Future<void> _buscarIncautos() async {
-    String matricula = _licensePlateController.text;
-    var response = await http.get(Uri.parse('http://localhost:8089/ciudadanos/$matricula'));
-     print('su inf:${response.body}');
-    if (response.statusCode == 200) {
+  Future<void> _getCitizen() async {
+    setState(() {
+      _isLoading = true;
+      _errorMessage = '';
+    });
+
+    String licensePlate = _licensePlateController.text;
+    try {
+var response = await http.get(Uri.parse('http://localhost:8089/ciudadanos/$licensePlate'));
+      if (response.statusCode == 200) {
+        setState(() {
+          _citizens = parseResponse(response.body);
+        });
+      } else if (response.statusCode == 404) {
+        setState(() {
+          _errorMessage = 'Citizen not found';
+        });
+      } else {
+        setState(() {
+          _errorMessage = 'Error: ${response.statusCode}';
+        });
+      }
+    } catch (e) {
       setState(() {
-        _incautos = parseResponse(response.body);
+        _errorMessage = 'Error: $e';
       });
-    } else {
-     
-     print('Error when making request: ${response.statusCode}');
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
     }
   }
 
-  List<Incauto> parseResponse(String responseBody) {
-  print("Respuesta JSON cruda: $responseBody");
-  
-  final parsed = json.decode(responseBody);
-  if (parsed['citizen'] != null) {
-    Incauto incauto = Incauto.fromJson(parsed['citizen']);
-    print("Objeto Incauto después del parseo: $incauto");
-    return [incauto];
-  } else {
-    print("No se encontró el objeto 'citizen' en la respuesta.");
-    return [];
+  List<Citizen> parseResponse(String responseBody) {
+    final parsed = json.decode(responseBody);
+    if (parsed['citizen'] != null) {
+      return [Citizen.fromJson(parsed['citizen'])];
+    } else {
+      return [];
+    }
   }
-}
+
   @override
-  Widget build(BuildContext context){ 
+  Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         primaryColor: Colors.blue,
@@ -60,7 +77,7 @@ class _MainAppState extends State<MainApp> {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Search Incautos'),
+          title: const Text('Search Citizens'),
           centerTitle: true,
         ),
         body: Padding(
@@ -71,54 +88,49 @@ class _MainAppState extends State<MainApp> {
               TextField(
                 controller: _licensePlateController,
                 decoration: const InputDecoration(
-                  labelText: 'Enter the Registration',
+                  labelText: 'Enter the License Plate',
                   border: OutlineInputBorder(),
                 ),
               ),
               const SizedBox(height: 16.0),
               ElevatedButton(
-                onPressed: _buscarIncautos,
+                onPressed: _isLoading ? null : _getCitizen,
+                child: _isLoading ? CircularProgressIndicator() : const Text('Search'),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Theme.of(context).colorScheme.primary,
                   padding: const EdgeInsets.symmetric(vertical: 16.0),
                 ),
-                child: const Text('Search'),
               ),
-               const SizedBox(height: 16.0),
-               Expanded(
-                child: _incautos.isNotEmpty
-                    ? ListView.builder(
-                        itemCount: _incautos.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Card(
-                            elevation: 8.0,
-                            margin: const EdgeInsets.symmetric(vertical: 8.0),                         
-                            child: ListTile(
-                              title: Column(
-                                children: [
-                                  Text(
-                                   _incautos[index].lat,
-                                    style: const TextStyle(fontWeight: FontWeight.bold),
-                                  ),Text(
-                                   _incautos[index].lon,
-                                    style: const TextStyle(fontWeight: FontWeight.bold),
-                                  ),Text(
-                                   _incautos[index].photo,
-                                    style: const TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  
-                                ],
-                              ),
-                              subtitle: Text(_incautos[index].licensePlate),
-                              // Agrega más elementos ListTile según los datos que quieras mostrar
-                            ),
-                          );
-                        },
-                      )
-                    : const Center(
-                        child: Text('No suspects were found'),
-                      ),
-              ),
+              const SizedBox(height: 16.0),
+              _isLoading
+                  ? CircularProgressIndicator()
+                  : _errorMessage.isNotEmpty
+                      ? Text(_errorMessage)
+                      : Expanded(
+                          child: _citizens.isNotEmpty
+                              ? ListView.builder(
+                                  itemCount: _citizens.length,
+                                  itemBuilder: (BuildContext context, int index) {
+                                    return Card(
+                                      elevation: 8.0,
+                                      margin: const EdgeInsets.symmetric(vertical: 8.0),
+                                      child: ListTile(
+                                        title: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text('License Plate: ${_citizens[index].licensePlate}'),
+                                            Text('Description: ${_citizens[index].description}'),
+                                            Text('Location: ${_citizens[index].lat}, ${_citizens[index].lon}'),
+                                          ],
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                )
+                              : const Center(
+                                  child: Text('No citizens found'),
+                                ),
+                        ),
             ],
           ),
         ),
@@ -127,24 +139,25 @@ class _MainAppState extends State<MainApp> {
   }
 }
 
-class Incauto {
-  final String photo;
+class Citizen {
   final String licensePlate;
+  final String description;
   final String lat;
   final String lon;
 
-  Incauto({
-    required this.photo,
+  Citizen({
     required this.licensePlate,
+    required this.description,
     required this.lat,
     required this.lon,
   });
 
-  factory Incauto.fromJson(Map<String, dynamic> json) {
-    return Incauto(
-      photo: json['photo'] as String,
+  factory Citizen.fromJson(Map<String, dynamic> json) {
+    return Citizen(
       licensePlate: json['licensePlate'] as String,
-      lat: json['location']['lat'] as String,
-      lon: json['location']['lon'] as String,
+      description: json['description'] as String,
+      lat: json['lat'] as String,
+      lon: json['lon'] as String,
     );
-  }}
+  }
+}
