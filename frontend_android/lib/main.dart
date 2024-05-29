6e8a289d8cdc60a907c489dfa259231ad0f24192
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-05-28 23:46:30 -0400

    _04_splash_Final

--- frontend_android/lib/main.dart
+++ frontend_android/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend_android/presentation/00_splash/_00_splash.dart';
+//import 'package:frontend_android/presentation/00_splash/_00_splash.dart';
 import 'package:frontend_android/presentation/screen_00_login/login_screen_p0.dart';
 import 'package:frontend_android/presentation/screen_01_camera/camera_screen_p1.dart';
 
@@ -11,15 +11,11 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        // Retiramos el debug icon
         debugShowCheckedModeBanner: false,
-        // Ruta por defecto
-        initialRoute: '/splash',
-        // rutas de acceso
+        initialRoute: '/login',
         routes: {
-           '/splash': (context) => const SplashScreen(),
-           '/login': (context) => Login(),
-          // Solo la muestra si el login es exitoso
+       //    '/splash': (context) => const SplashScreen(),
+          '/login': (context) => Login(),
           '/camera': (context) => const Camera(),
         });
   }
