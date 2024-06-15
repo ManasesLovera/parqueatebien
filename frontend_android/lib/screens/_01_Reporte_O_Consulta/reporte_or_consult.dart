df0c5740142a3b0d1b6a5d39723cf18c9e57d304
x <erickdavestech@gmail.com>
2024-06-15 16:32:48 -0400

    WelcomeReportOrConsult_Clean

--- frontend_android/lib/screens/_01_Reporte_O_Consulta/reporte_or_consult.dart
+++ frontend_android/lib/screens/_01_Reporte_O_Consulta/reporte_or_consult.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:frontend_android/Services/_01_Reporte_O_Consulta/Controllers/navigation_controller.dart';
+import 'package:frontend_android/Services/_01_Reporte_O_Consulta/widgets/_03_reportorconsultbuttom.dart';
+import 'package:frontend_android/Services/_01_Reporte_O_Consulta/widgets/_00_mainimage.dart';
+import 'package:frontend_android/Services/_01_Reporte_O_Consulta/widgets/_02_subtituloreport.dart';
+import 'package:frontend_android/Services/_01_Reporte_O_Consulta/widgets/_01_welcometext.dart';
 
 class ReportorConsult extends StatelessWidget {
   const ReportorConsult({super.key});
@@ -16,122 +20,30 @@ class ReportorConsult extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               SizedBox(height: 30.h),
-              Image.asset(
-                'assets/whiteback/main_w.png',
-                height: 50.h,
-              ),
+              const ImageReportConsult(),
               SizedBox(height: 50.h),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 12.h),
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Text(
-                    'Bienvenido',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 26.h,
-                    ),
-                  ),
-                ),
-              ),
+              const WelcomeText(),
               SizedBox(height: 3.h),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 14.h),
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Text(
-                    '¿Que deseas realizar hoy?',
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 13.h,
-                    ),
-                  ),
-                ),
-              ),
+              const SubtituloReport(),
               SizedBox(height: 30.h),
-              ReportButton(
+              ReportConsultButtom(
                 svgPath: 'assets/icons/create.svg',
                 title: 'Crear reporte',
                 subtitle:
                     'Crea un nuevo reporte de un vehículo mal estacionado.',
                 onTap: () {
-                  Navigator.of(context).pop();
-                  Navigator.pushNamed(context, '/report');
+                  NavigationController.navigateToNewReport(context);
                 },
               ),
               SizedBox(height: 12.h),
-              ReportButton(
+              ReportConsultButtom(
                 svgPath: 'assets/icons/car.svg',
                 title: 'Consultar placa',
                 subtitle:
                     'Consulta el estatus y ubicación de un vehículo incautado.',
                 onTap: () {
-                  Navigator.of(context).pop();
-                  Navigator.pushNamed(context, '/consult');
+                  NavigationController.navigateToConsult(context);
                 },
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ReportButton extends StatelessWidget {
-  final String svgPath;
-  final String title;
-  final String subtitle;
-  final VoidCallback onTap;
-
-  const ReportButton({
-    super.key,
-    required this.svgPath,
-    required this.title,
-    required this.subtitle,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 2.h),
-        child: Container(
-          padding: EdgeInsets.all(8.h),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.blue, width: 2.h),
-            borderRadius: BorderRadius.circular(12.h),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SvgPicture.asset(svgPath, height: 35.h),
-              SizedBox(width: 8.h),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      title,
-                      style: TextStyle(
-                        fontSize: 14.h,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blue,
-                      ),
-                    ),
-                    Text(
-                      subtitle,
-                      style: TextStyle(
-                        fontSize: 9.h,
-                        color: Colors.grey[600],
-                      ),
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
-                ),
               ),
             ],
           ),
