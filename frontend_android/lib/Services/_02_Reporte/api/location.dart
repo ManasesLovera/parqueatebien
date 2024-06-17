e4de1f632d9572849064a58e79146e6f3761489a
x <erickdavestech@gmail.com>
2024-06-17 14:11:41 -0400

    google_maps_working_map_source_to_destination_

--- frontend_android/lib/Services/_02_Reporte/api/location.dart
+++ frontend_android/lib/Services/_02_Reporte/api/location.dart
@@ -1,34 +1,21 @@
 import 'package:geolocator/geolocator.dart';
-import 'package:logger/logger.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class LocationService {
-  final Logger _logger = Logger();
-
-  Future<bool> _requestLocationPermission() async {
-    PermissionStatus status = await Permission.location.request();
-    return status == PermissionStatus.granted;
-  }
-
   Future<Position?> getCurrentLocation() async {
-    try {
-      bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-      if (!serviceEnabled) {
-        _logger.e('Location services are disabled.');
-        return null;
-      }
-
-      bool permissionGranted = await _requestLocationPermission();
-      if (!permissionGranted) {
-        _logger.e('Location permissions are denied.');
-        return null;
-      }
-
-      return await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high);
-    } catch (e) {
-      _logger.e('Error retrieving location: $e');
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
       return null;
     }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return null;
+      }
+    }
+
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
   }
 }
