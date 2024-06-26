f14975d71a3ef24fe369f44e2e7f1e5167a0556a
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-06-25 23:13:28 -0700

    bloc_vehicule_details

--- frontend_android_ciudadano/lib/Data/Api/ConsultaDePlacas/_00_api_consulta_placa.dart
+++ frontend_android_ciudadano/lib/Data/Api/ConsultaDePlacas/_00_api_consulta_placa.dart
@@ -1,20 +1,47 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
 
 class ConsultaPlaca {
-  final String apiUrl =
+  final String apiUrlList =
       'https://parqueatebiendemo.azurewebsites.net/ciudadanos/ciudadanos';
+  final String apiUrlDetails =
+      'https://parqueatebiendemo.azurewebsites.net/ciudadanos/';
+  final Logger _logger = Logger();
 
   Future<List<String>> fetchLicencePlates() async {
-    final response = await http.get(Uri.parse(apiUrl));
+    final response = await http.get(Uri.parse(apiUrlList));
+    _logger.i('Fetching licence plates from $apiUrlList');
 
     if (response.statusCode == 200) {
       List<dynamic> data = json.decode(response.body);
-      List<String> licensePlate =
+      List<String> licensePlates =
           data.map((item) => item['licensePlate'] as String).toList();
-      return licensePlate;
+      _logger.i('Fetched licence plates: $licensePlates');
+      return licensePlates;
     } else {
+      _logger.e(
+          'Failed to load licence plates with status code ${response.statusCode}');
       throw Exception('Failed to load licence plates');
+    }
+  }
+
+  Future<Map<String, dynamic>> fetchVehicleDetails(String licensePlate) async {
+    final response = await http.get(Uri.parse('$apiUrlDetails$licensePlate'));
+    _logger.i(
+        'Fetching details for plate: $licensePlate from $apiUrlDetails$licensePlate');
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(response.body);
+      _logger.i('Fetched vehicle details: $data');
+      return data;
+    } else if (response.statusCode == 404) {
+      _logger.w('Vehicle not found for plate: $licensePlate');
+      throw Exception('Vehicle not found');
+    } else {
+      _logger.e(
+          'Failed to load vehicle details with status code ${response.statusCode}');
+      throw Exception('Failed to load vehicle details');
     }
   }
 }
--- frontend_android_ciudadano/lib/Data/Blocs/Vehiculo/_00_vehicle_event.dart
+++ frontend_android_ciudadano/lib/Data/Blocs/Vehiculo/_00_vehicle_event.dart
@@ -8,3 +8,21 @@ abstract class VehicleEvent extends Equatable {
 }
 
 class FetchLicencePlates extends VehicleEvent {}
+
+class SelectLicencePlate extends VehicleEvent {
+  final String selectedPlate;
+
+  const SelectLicencePlate(this.selectedPlate);
+
+  @override
+  List<Object> get props => [selectedPlate];
+}
+
+class FetchVehicleDetails extends VehicleEvent {
+  final String selectedPlate;
+
+  const FetchVehicleDetails(this.selectedPlate);
+
+  @override
+  List<Object> get props => [selectedPlate];
+}
