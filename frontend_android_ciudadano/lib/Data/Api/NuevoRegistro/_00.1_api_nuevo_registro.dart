708e14ae5fad55f2fb9fe3662f0975d7e04e97ad
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-06-25 21:23:11 -0700

    restruc

---
+++ frontend_android_ciudadano/lib/Data/Api/NuevoRegistro/_00.1_api_nuevo_registro.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:frontend_android_ciudadano/Data/Models/NuevoRegistro/User_Extention/user_extensions.dart';
+import 'package:frontend_android_ciudadano/Data/Models/NuevoRegistro/Users_Model/user_model.dart';
+import 'package:http/http.dart' as http;
+
+class RegisterApi {
+  Future<bool> register(User user) async {
+    final response = await http.post(
+      Uri.parse('https://api.tuapp.com/register'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(user.toJson()),
+    );
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
---
+++ frontend_android_ciudadano/lib/Data/Blocs/Login/ButtomLoginState/sign_in_bloc_builder.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:frontend_android_ciudadano/Data/Blocs/Login/ButtomLoginState/button_state_bloc.dart';
+import 'package:frontend_android_ciudadano/Data/Blocs/Login/LoginLogic/_00_login_event.dart';
+import 'package:frontend_android_ciudadano/Data/Blocs/Login/LoginLogic/_01_login_state.dart';
+import 'package:frontend_android_ciudadano/Data/Blocs/Login/LoginLogic/_02_login_bloc.dart';
+import 'package:frontend_android_ciudadano/Views/Welcome/_00_welcome.dart';
+import 'package:frontend_android_ciudadano/Widgets/NuevoRegistro/_02_custom_buttom_.dart';
+
+class SignInBlocBuilder extends StatelessWidget {
+  final TextEditingController iD;
+  final TextEditingController pass;
+
+  const SignInBlocBuilder({
+    required this.iD,
+    required this.pass,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<LoginBloc, LoginState>(listener: (context, state) {
+      if (state is LoginSuccess) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => const Welcome(),
+          ),
+        );
+      }
+    }, child: BlocBuilder<LoginBloc, LoginState>(
+      builder: (context, state) {
+        return Column(
+          children: [
+            if (state is LoginLoading)
+              const CircularProgressIndicator()
+            else
+              BlocBuilder<ButtonStateBloc, ButtonState>(
+                builder: (context, buttonState) {
+                  return RegistroButtom(
+                    onPressed: buttonState.isEnabled
+                        ? () {
+                            context.read<LoginBloc>().add(
+                                  LoginSubmitted(
+                                    iD.text,
+                                    pass.text,
+                                  ),
+                                );
+                          }
+                        : null,
+                    isEnabled: buttonState.isEnabled,
+                    text: 'Ingresar',
+                  );
+                },
+              ),
+            SizedBox(height: 4.h),
+            if (state is LoginFailure)
+              Column(
+                children: [
+                  Text(
+                    state.error,
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 16.h,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  FutureBuilder(
+                    future: Future.delayed(const Duration(seconds: 5)),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        context.read<LoginBloc>().add(ClearError());
+                      }
+                      return Container();
+                    },
+                  ),
+                ],
+              ),
+          ],
+        );
+      },
+    ));
+  }
+}
