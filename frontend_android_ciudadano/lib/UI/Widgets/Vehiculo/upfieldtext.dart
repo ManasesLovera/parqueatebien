e5088171fedbe4f3f5456ba971f6a6852075662f
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-06-26 21:44:54 -0400

    report_info_working_colors

--- frontend_android_ciudadano/lib/UI/Widgets/Vehiculo/upfieldtext.dart
+++ frontend_android_ciudadano/lib/UI/Widgets/Vehiculo/upfieldtext.dart
@@ -1,17 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Upfields extends StatelessWidget {
   final String? text;
   const Upfields({
-    super.key, this.text,
+    super.key,
+    this.text,
   });
 
   @override
   Widget build(BuildContext context) {
     return Text(
-      text ??'',
+      text ?? '',
       style: TextStyle(
+        color: Colors.blue,
         fontSize: 11.h,
         fontWeight: FontWeight.bold,
       ),
