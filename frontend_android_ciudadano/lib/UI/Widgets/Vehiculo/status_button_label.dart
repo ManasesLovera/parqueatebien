36ce0ff461f6d622c7e1c1da8a0ba231c5a79f4c
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-06-26 20:33:46 -0400

    refactor_code_00_add_status_change

--- frontend_android_ciudadano/lib/UI/Widgets/Vehiculo/status_button_label.dart
+++ frontend_android_ciudadano/lib/UI/Widgets/Vehiculo/status_button_label.dart
@@ -9,13 +9,28 @@ class SatusButtom extends StatelessWidget {
 
   final Map<String, dynamic> details;
 
+  Color _getStatusColor(String status) {
+    switch (status.toLowerCase()) {
+      case 'reportado':
+        return Colors.grey;
+      case 'retenido':
+        return Colors.red;
+      case 'liberado':
+        return Colors.green;
+      case 'incautado por grua':
+        return Colors.orange;
+      default:
+        return Colors.grey;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: ElevatedButton(
         onPressed: () {},
         style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.red,
+          backgroundColor: _getStatusColor(details['Status']),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(20),
           ),
