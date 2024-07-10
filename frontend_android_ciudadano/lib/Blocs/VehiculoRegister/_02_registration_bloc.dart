43fadf276b624bcfb3f3bcb3ae461000b94bfb0b
Daves <54048831+erickdavestech@users.noreply.github.com>
2024-07-10 17:08:46 -0400

    restruct_

---
+++ frontend_android_ciudadano/lib/Blocs/VehiculoRegister/_02_registration_bloc.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend_android_ciudadano/Api/Add_Vehiculo/add_vechicle_api.dart';
+import 'package:frontend_android_ciudadano/Blocs/VehiculoRegister/_00_registration_event.dart';
+import 'package:frontend_android_ciudadano/Blocs/VehiculoRegister/_01_registration_state.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class VehicleRegistrationBloc
+    extends Bloc<VehicleRegistrationEvent, VehicleRegistrationState> {
+  final AddVehicleApi api;
+
+  VehicleRegistrationBloc(this.api) : super(VehicleRegistrationInitial()) {
+    on<RegisterVehicle>((event, emit) async {
+      emit(VehicleRegistrationLoading());
+      try {
+        final success = await api.addVehicle(
+          governmentId: event.governmentId,
+          licensePlate: event.licensePlate,
+          registrationDocument: event.matricula,
+          model: event.model,
+          year: event.year,
+          color: event.vehicleColor,
+        );
+        if (success) {
+          emit(VehicleRegistrationSuccess());
+        } else {
+          emit(const VehicleRegistrationFailure(error: 'Ya tiene un Vehiculo con esta placa'));
+        }
+      } catch (error) {
+        emit(VehicleRegistrationFailure(error: error.toString()));
+      }
+    });
+
+    on<FetchGovernmentId>((event, emit) async {
+      final prefs = await SharedPreferences.getInstance();
+      final governmentId = prefs.getString('governmentId');
+      if (governmentId != null) {
+        add(RegisterVehicle(
+          governmentId: governmentId,
+          licensePlate: event.licensePlate,
+          vehicleType: event.vehicleType,
+          vehicleColor: event.vehicleColor,
+          model: event.model,
+          year: event.year,
+          matricula: event.matricula,
+        ));
+      } else {
+        emit(const VehicleRegistrationFailure(
+            error: 'No se encontró el ID del usuario'));
+      }
+    });
+  }
+}
---
+++ frontend_android_ciudadano/lib/Pages/Login/_00_login.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:frontend_android_ciudadano/Blocs/Login/ButtomLoginState/buttom_is_or_not_enable.dart';
+import 'package:frontend_android_ciudadano/Blocs/Login/ButtomLoginState/sign_in_bloc_builder.dart';
+import 'package:frontend_android_ciudadano/Blocs/Login/Login_Bloc/_02_login_bloc.dart';
+import 'package:frontend_android_ciudadano/Pages/User_Login_Register_User_With_Vehicle/_00.0_user.dart';
+import 'package:frontend_android_ciudadano/Widgets/GlobalsWidgets/_00_logo_image.dart';
+import 'package:frontend_android_ciudadano/Widgets/Login/_01_user_text_for_textfield_.dart';
+import 'package:frontend_android_ciudadano/Widgets/Login/_05_forgot_password_tex.dart';
+import 'package:frontend_android_ciudadano/Widgets/Login/_06_not_account_text.dart';
+import 'package:frontend_android_ciudadano/Widgets/Login/_09_custom_registrate_now.dart';
+import 'package:frontend_android_ciudadano/Widgets/NuevoRegistro/_01_custom_textfield_.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => LoginState();
+}
+
+class LoginState extends State<Login> {
+  final iD = TextEditingController();
+  final pass = TextEditingController();
+  //
+  bool obscureText = true;
+  //
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 9, 157, 210),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 0.h),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: 50.h),
+                const CustomImageLogo(
+                    img: 'assets/splash/main.png', altura: 100),
+                SizedBox(height: 50.h),
+                const Usertext(
+                  text: 'Cedula',
+                ),
+                BlocProvider(
+                  create: (_) => ButtonStateBloc(),
+                  child: Builder(
+                    builder: (context) {
+                      iD.addListener(() {
+                        context.read<ButtonStateBloc>().add(
+                              TextChanged(iD.text, pass.text),
+                            );
+                      });
+                      pass.addListener(() {
+                        context.read<ButtonStateBloc>().add(
+                              TextChanged(iD.text, pass.text),
+                            );
+                      });
+
+                      return Column(
+                        children: [
+                          CustomTextField(
+                            controller: iD,
+                            hintText: 'Ingresar cedula',
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly,
+                              CedulaFormatterCedula(),
+                            ],
+                          ),
+                          SizedBox(height: 20.h),
+                          const Usertext(
+                            text: 'Contraseña',
+                          ),
+                          CustomTextField(
+                            controller: pass,
+                            hintText: 'Ingresar la contraseña',
+                            obscureText: obscureText,
+                            //
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                obscureText
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  obscureText = !obscureText;
+                                });
+                              },
+                            ),
+                            //
+                          ),
+                          SizedBox(height: 16.h),
+                          BlocProvider(
+                            create: (_) => LoginBloc(),
+                            child: SignInBlocBuilder(
+                              iD: iD,
+                              pass: pass,
+                            ),
+                          ),
+                          SizedBox(height: 15.h),
+                          const ForgotPasswordText(
+                            text: '¿Olvidaste tu Contraseña?',
+                          ),
+                          SizedBox(height: 80.h),
+                          const DontAccount(
+                            text: '¿No tienes una cuenta?',
+                          ),
+                          SizedBox(height: 8.h),
+                          SizedBox(
+                            child: RegisterNow(
+                              onPressed: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => const RegisterUser(),
+                                  ),
+                                );
+                              },
+                              text: 'Registrarse ahora',
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CedulaFormatterCedula extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    String text = newValue.text.replaceAll('-', ''); // Remove existing dashes
+    if (text.length > 11) {
+      return oldValue; // Limit to 11 characters
+    }
+
+    StringBuffer buffer = StringBuffer();
+    for (int i = 0; i < text.length; i++) {
+      if (i == 3 || i == 10) {
+        buffer.write('-');
+      }
+      buffer.write(text[i]);
+    }
+
+    return TextEditingValue(
+      text: buffer.toString(),
+      selection: newValue.selection.copyWith(
+        baseOffset: buffer.length,
+        extentOffset: buffer.length,
+      ),
+    );
+  }
+}
